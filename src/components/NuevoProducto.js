53288ebbaac061f7691034a5edddf0669e0a87f2
victormmorales <victor.morales@criptan.com>
2022-01-26 11:11:37 +0100

    form

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,5 +1,46 @@
 const NuevoProducto = () => {
-  return <h1>Nuevo</h1>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form>
+              <div className="form-group">
+                <label>Nombre</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Precio</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio - €"
+                  name="precio"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NuevoProducto;
